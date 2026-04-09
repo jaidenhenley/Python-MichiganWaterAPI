85033bca3e072d91c35661871f3dd9c0f01763e7
jaidenhenley <jaidenhenley@gmail.com>
2026-04-08 20:51:17 -0400

    [Feature] Updated holiday service to check for US holidays in Michigan
    - Replaced external API call with local holiday data using the `holidays` library
    - Added function `is_holiday` to check if a given date is a holiday in Michigan, USA

--- app/services/holidays.py
+++ app/services/holidays.py
@@ -1,37 +1,6 @@
-from typing import Optional
-from fastapi import HTTPException
+import holidays
+from datetime import date
 
-import httpx
-
-from app.models import Holiday
-
-async def fetch_holidays(year: int, countryCode: str):
-    url = f"https://date.nager.at/api/v3/publicholidays/{year}/{countryCode}"
-
-    async with httpx.AsyncClient(timeout=10.0) as client:
-        response = await client.get(url,)
-
-
-    if response.status_code >= 500:
-        raise HTTPException(
-            status_code=502, detail="Traffic service is unavailable right now"
-        )
-    
-    response.raise_for_status()
-
-    data = response.json()
-    return data
-
-def parse_holiday(data: Optional[dict]) -> list:
-    if not data: 
-        return []
-    
-
-    return [
-        Holiday(
-        date=item["date"],
-        localName=item["localName"],
-        name=item["name"],
-    )
-    for item in data
-    ]
+def is_holiday(check_date: date) -> bool:
+    mi_holidays = holidays.US(state="MI")
+    return check_date in mi_holidays
